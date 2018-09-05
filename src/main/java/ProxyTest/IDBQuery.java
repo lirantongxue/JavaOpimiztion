17d3f7bddde513ba0dda8545744bb268fba05910
liran <644266618@qq.com>
2018-09-05 20:35:29 +0800

    修改了SSL 指向的域名地址

---
+++ src/main/java/ProxyTest/IDBQuery.java
@@ -0,0 +1,15 @@
+package ProxyTest;
+
+/**
+ * 方法
+ * @Auth liran
+ * @Create 2018/8/5 下午2:29
+ * @Param
+ * @Return
+ * @Description 主题接口
+ */
+public interface IDBQuery {
+
+    String request();
+
+}
